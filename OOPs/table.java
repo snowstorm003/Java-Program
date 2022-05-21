71df79f6ae3fbaa37ed88eadff5b8ffc73a5fe89
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2022-05-21 18:36:58 +0530

    table updated

--- OOPs/table.java
+++ OOPs/table.java
@@ -1,4 +1,32 @@
+import java.util.*;
 public class table 
 {
-    
+    int n, i;
+    table()
+    {
+        n=0;
+        i=0;
+    }
+
+    int accept()
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print(" Enter number : ");
+        n=sc.nextInt();
+
+        sc.close();
+        return n;
+    }
+
+    void multiply(int num)
+    {
+        for(i=1; i<=10; i++)
+            System.out.println(" "+num+"X"+i+"="+(num*i));
+    }
+    public static void main(String args[])
+    {
+        table ob = new table();
+        ob.multiply(ob.accept());
+    }
 }
