cd9067d614480b2ca15ba6d5e02e0f69ddbaf3b7
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2022-05-21 20:22:17 +0530

    fibo updated

--- OOPs/fibonacci.java
+++ OOPs/fibonacci.java
@@ -2,41 +2,34 @@ import java.util.Scanner;
 
 public class fibonacci 
 {
-    int n, i, a, b, c;
-
+    int n;
     fibonacci() 
     {
         n = 0;
-        a = 0;
-        b = 0;
-        c = 0;
-        i = 0;
     }
 
-    int accept() 
+    void accept() 
     {
         Scanner sc = new Scanner(System.in);
+        int i = 0;
         while (i == 0) 
         {
             System.out.print(" Enter number : ");
             n = sc.nextInt();
 
-            if (n <= 0) 
-            {
+            if (n <= 0)
                 System.out.println(" Invalid Input!!!!!");
-                i = 0;
-            } else
+            else
                 i = 1;
         }
 
         sc.close();
-        return n;
     }
 
-    void fibo(int num) 
+    void fibo() 
     {
-        b = 1;
-        for (i = 1; i <= num; i++) 
+        int a = 0, b = 1, c = 0;
+        for (int i = 1; i <= n; i++) 
         {
             System.out.println(" " + a);
             c = a + b;
@@ -50,7 +43,8 @@ public class fibonacci
     public static void main(String args[]) 
     {
         fibonacci ob = new fibonacci();
-        ob.fibo(ob.accept());
+        ob.accept();
+        ob.fibo();
 
     }
 
