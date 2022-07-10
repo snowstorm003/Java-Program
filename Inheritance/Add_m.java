c269447346ed884c4d203a2e0136e7d1639ff16f
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2022-07-10 21:45:42 +0530

    sum add

---
+++ Inheritance/Add_m.java
@@ -0,0 +1,12 @@
+
+public class Add_m
+{
+    public static void main()
+    {
+        Add ob = new Add();
+        ob.input();
+        ob.addition();
+        ob.print();
+
+    }
+}
--- Inheritance/add.java
+++ Inheritance/add.java
@@ -1,3 +1,36 @@
-public class add {
-    
+import java.util.*;
+public class Add 
+{
+    int sum,a, b;
+    Add()
+    {
+        sum=0;
+        a=0;
+        b=0;
+    }
+    public void input()
+    {
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sc.close();
+    }
+    public void addition()
+    {
+        sum=a;
+        for(int i=a; i<=b; i++)
+        {
+            sum++;
+        }
+        
+    }
+    public void print()
+    {
+        System.out.println(" Sum : "+sum);
+    }
 }
+
+
+    
+
+
